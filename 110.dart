a45b3e10fc308337b3f5c731c9e478e8fcc16f4c
HassemMD <hassemmohammed90@gmail.com>
2025-07-19 18:31:45 +0530

    added

---
+++ 110.dart
@@ -0,0 +1,14 @@
+void apple(){
+  print("this is apple");
+}
+void samsung(){
+  print("this is samsung");
+}
+void redmi(){
+  print("this is redmi");
+}
+void main(){
+  apple();
+  samsung();
+  redmi();
+}
