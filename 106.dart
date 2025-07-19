6b13b33bb9d3a02cfe761ed99cec482bb7c999be
HassemMD <hassemmohammed90@gmail.com>
2025-07-20 02:28:27 +0530

    added

--- 106.dart
+++ 106.dart
@@ -1,5 +1,6 @@
 void main()
 {
+  // ignore: unused_local_variable
   String fruit = "orange";
   int count = 20;
   int price = 100;
--- 107.dart
+++ 107.dart
@@ -1,11 +1,13 @@
 void main()
 {
+  // ignore: unused_local_variable
   bool holiday = true;
   if(true)
   {
     print("go out with friends");
   }
     else
+    // ignore: dead_code
     {
       print("get ready to go to college");
     }
